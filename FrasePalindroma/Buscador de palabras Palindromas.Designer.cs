f1552390fb8c590a144310be37e3a792ecbae7df
BastianPaez <49958062+BastianPaez@users.noreply.github.com>
2019-05-20 18:56:36 -0400

    Trabajo
    20/05

---
+++ FrasePalindroma/Buscador de palabras Palindromas.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace FrasePalindroma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnValidar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtFrase = new System.Windows.Forms.TextBox();
+            this.CantidadLetras = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnValidar
+            // 
+            this.btnValidar.Location = new System.Drawing.Point(242, 200);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(180, 23);
+            this.btnValidar.TabIndex = 0;
+            this.btnValidar.Text = "Validar Frase";
+            this.btnValidar.UseVisualStyleBackColor = true;
+            this.btnValidar.Click += new System.EventHandler(this.BtnValidar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label1.Location = new System.Drawing.Point(256, 140);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ingrese una frase.";
+            // 
+            // TxtFrase
+            // 
+            this.TxtFrase.Location = new System.Drawing.Point(85, 160);
+            this.TxtFrase.MaxLength = 100;
+            this.TxtFrase.Name = "TxtFrase";
+            this.TxtFrase.Size = new System.Drawing.Size(530, 20);
+            this.TxtFrase.TabIndex = 2;
+            this.TxtFrase.TextChanged += new System.EventHandler(this.TxtFrase_TextChanged);
+            // 
+            // CantidadLetras
+            // 
+            this.CantidadLetras.AutoSize = true;
+            this.CantidadLetras.Location = new System.Drawing.Point(621, 163);
+            this.CantidadLetras.Name = "CantidadLetras";
+            this.CantidadLetras.Size = new System.Drawing.Size(13, 13);
+            this.CantidadLetras.TabIndex = 3;
+            this.CantidadLetras.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(640, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "/100";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 32.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, -2);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(719, 51);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Contador de palabras Palindromas";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(718, 311);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.CantidadLetras);
+            this.Controls.Add(this.TxtFrase);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnValidar);
+            this.Name = "Form1";
+            this.Text = "Contador de Palindromas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnValidar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtFrase;
+        private System.Windows.Forms.Label CantidadLetras;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ FrasePalindroma/Buscador de palabras Palindromas.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrasePalindroma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void TxtFrase_TextChanged(object sender, EventArgs e)
+        {
+            //contador de letras
+            String frase = TxtFrase.Text.Replace(" ", "");
+            int tope = 101;
+            int NumeroFrase = frase.Length;
+
+            for (int ConteoLetras = 0; ConteoLetras <= NumeroFrase; ConteoLetras++)
+            {
+                if (ConteoLetras == tope)
+                {
+                    ConteoLetras--;
+                    MessageBox.Show("No se permite mas de 100 letras");
+                    break;
+                }
+                CantidadLetras.Text = ConteoLetras.ToString();
+            }
+        }
+        private void BtnValidar_Click(object sender, EventArgs e)
+        {
+            ContadorDePalindromas();
+        }
+
+        public bool SiEsPalindroma(string Palabras)
+        {
+            int Fin;
+            bool EsPalindroma= true;
+
+            if (!string.IsNullOrWhiteSpace(Palabras))
+            {   
+                if(Palabras.Length > 1)
+                {int Mitad = Palabras.Length / 2;
+                Fin = Palabras.Length - 1;
+                for (int inicio = 0; inicio < Mitad; inicio++)
+                {
+                    if (Palabras[inicio] != Palabras[Fin])
+                    {
+                        EsPalindroma = false;
+                        break;
+                    }
+                    Fin--;
+                }
+                    if (EsPalindroma)
+                    {
+                        return true;
+                    }
+                }
+        }
+            return false;
+        }
+            
+
+        private int conteo = 0;
+
+            private void ContadorDePalindromas()
+            {
+                string[] Frase = TxtFrase.Text.ToLower().Split(' ');
+
+                this.conteo = 0;
+
+                foreach (var UnaPalabra in Frase)
+                {
+                    if (SiEsPalindroma(UnaPalabra))
+                    {
+                        this.conteo++;
+                    }
+                }
+                MessageBox.Show("son " + conteo+ " palabras Palindromas.");
+            }
+
+
+
+    }
+}
